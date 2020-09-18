07e018dc4221b34ef59269e7418aba66b1fd61de
Nick Rose <nickfbp@gmail.com>
2020-09-18 10:22:20 -0600

    Added Contact Route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { INDEX } from '../shared/homepage';
 
@@ -29,6 +30,7 @@ class Main extends Component {
             <Switch>
                 <Route path = '/home' component={HomePage} />
                 <Route exact path='/directory' render={() => <Directory item = {this.state.index} />} />
+                <Route exact path='/contactus' component={Contact} />
                 <Redirect to='/home' />
             </Switch>
             <Footer />
