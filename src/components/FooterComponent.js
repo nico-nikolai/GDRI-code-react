2a2132b7b41f52379befaff648fb7b5f24add86b
Nick Rose <nickfbp@gmail.com>
2020-09-22 16:49:29 -0600

    Updated styles

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -24,7 +24,7 @@ function Footer(props) {
                     </div>
                     <div className="col-sm-4 text-center">
                         <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
+                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> nickfbp@gmail.com</a>
                     </div>
                 </div>
             </div>
